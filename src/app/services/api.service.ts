6d562723dfedd2319869547fb172996116cf466e
M.can <caglayan.mertcan37@gmail.com>
2025-01-01 16:26:50 +0300

    test5

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = environment.APIURL!;
+  private apiUrl = environment.apiUrl!;
 
   constructor(private http: HttpClient) {}
 
